0e01d54e66d591392f09c2c223bf7cec625e9ab1
Menna Ibrahim <mennaibrahim815@gmail.com>
2025-04-21 21:58:40 +0200

    Add files via upload
    Add Footer

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Footer = () => (
+  <footer className="w-full py-4 text-center text-xs text-[#B0B3B6] border-t border-[#ECECEC] bg-white">
+    © 2025 OnTime. All rights reserved.
+  </footer>
+);
+
+export default Footer;
